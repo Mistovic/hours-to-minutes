dc2e139d5eaf3eee94c6f3fb6d6af63bb95d7be4
Vuk Mistovic <vuk93@ymail.com>
2021-02-27 22:42:13 +0100

    Addititonal styling

--- js/converter.js
+++ js/converter.js
@@ -2,17 +2,40 @@
     var inputMinutes = document.getElementById('minutes');
     var inputHours = document.getElementById('hours');
 
-    function toHours() {
-        let valMinutes = this.value;
+    function toHours(input) {
+        let valMinutes = input.value;
         let hours = Math.round(valMinutes) / 60;
         inputHours.value = hours.toFixed(2)
     }
 
-    function toMinutes() {
-        let valHours = this.value;
+    function toMinutes(input) {
+        let valHours = input.value;
         inputMinutes.value = valHours * 60;
     }
 
-    inputMinutes.addEventListener('input', toHours);
-    inputHours.addEventListener('input', toMinutes);
+    function animation(input) {
+        let val = input.value;
+        console.log('Blur', val)
+        let parent = input.parentNode;
+        let label = input.nextElementSibling;
+        if (val != 0 || val != '') {
+            label.style = "font-size: 1.5rem"
+            console.log('Jesmo li dobrii', label)
+        } else {
+            label.style = "font-size: 4rem";
+
+        }
+    }
+
+
+    // Input event
+    inputMinutes.addEventListener('input', function () {
+        toHours(this);
+        animation(this);
+    });
+    inputHours.addEventListener('input', function () {
+        toMinutes(this);
+        animation(this);
+    });
+
 })();
